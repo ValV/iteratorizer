e95eae62497e63af08e56f1dd9678f5e1fae8aed
Vladimir Valeyev <ValV@users.noreply.github.com>
2019-06-21 10:58:16 +0600

    Commit second approach (getNext method, fixed size)

--- src/main/kotlin/iteratorizer/App.kt
+++ src/main/kotlin/iteratorizer/App.kt
@@ -4,33 +4,45 @@
 
 package iteratorizer
 
-class App {
-  val lists = listOf(
-    listOf(1, 8, 12, 17, 26, 31),
-    listOf(2, 7, 35, 40),
-    listOf(10, 13, 15, 18)
-  )
+class App(val lists: List<List<Int>>) {
+  private val itX: List<Iterator<Int>> by lazy {
+    listOf(
+        lists[0].iterator(),
+        lists[1].iterator(),
+        lists[2].iterator()
+      )
+  }
+  private val current: MutableList<Int> by lazy {
+    mutableListOf(itX[0].next(), itX[1].next(), itX[2].next())
+  }
+  private val inf = Int.MAX_VALUE
+  private var count = 0
+  private val size = 3
+  private var idx = 0
+
+  init {
+    count = lists[0].size + lists[1].size + lists[2].size
+  }
+
+  fun getNext() {
+    if (current[idx] == current.min() && hasNext()) {
+      println("${current[idx]}\t$current")
+      current[idx] = if (itX[idx].hasNext()) itX[idx].next() else inf
+      count --
+    }
+    idx = if (idx == size - 1) 0 else idx + 1
+  }
+
+  fun hasNext() = count > 0
 }
 
 fun main(args: Array<String>) {
-  val app = App()
-  val itX = listOf(
-    app.lists[0].iterator(),
-    app.lists[1].iterator(),
-    app.lists[2].iterator()
-  )
-  val current = mutableListOf(itX[0].next(), itX[1].next(), itX[2].next())
-  var count = app.lists[0].size + app.lists[1].size + app.lists[2].size
-  val inf = Int.MAX_VALUE
-  while (count > 0) {
-    for (i in 0 .. 2) {
-      if (current[i] == current.min()) {
-        println("${current[i]}\t$current")
-        current[i] = if (itX[i].hasNext()) itX[i].next() else inf
-        if ((count --) == 1) break
-      }
-    }
-  }
+  val app = App(listOf(
+    listOf(1, 8, 12, 17, 26, 31),
+    listOf(2, 7, 35, 40),
+    listOf(10, 13, 15, 18)
+  ))
+  while (app.hasNext()) app.getNext()
 }
 
 // vim: se et ts=2 sw=2 number:
