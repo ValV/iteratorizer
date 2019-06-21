2a4cb03e68c668b93012836a2a6e4583f8b3d64a
Vladimir Valeyev <ValV@users.noreply.github.com>
2019-06-21 11:43:41 +0600

    Commit third approach (getNext method, unlimited lists)

--- src/main/kotlin/iteratorizer/App.kt
+++ src/main/kotlin/iteratorizer/App.kt
@@ -6,26 +6,20 @@ package iteratorizer
 
 class App(val lists: List<List<Int>>) {
   private val itX: List<Iterator<Int>> by lazy {
-    listOf(
-        lists[0].iterator(),
-        lists[1].iterator(),
-        lists[2].iterator()
-      )
+    lists.map {it.iterator()}
   }
+ 
   private val current: MutableList<Int> by lazy {
-    mutableListOf(itX[0].next(), itX[1].next(), itX[2].next())
+    itX.mapTo(mutableListOf()) {it.next()}
   }
+ 
   private val inf = Int.MAX_VALUE
-  private var count = 0
-  private val size = 3
+  private val size = lists.size
+  private var count = lists.sumBy {it.size}
   private var idx = 0
 
-  init {
-    count = lists[0].size + lists[1].size + lists[2].size
-  }
-
   fun getNext() {
-    if (current[idx] == current.min() && hasNext()) {
+    if (current[idx] == current.min()/* && hasNext()*/) {
       println("${current[idx]}\t$current")
       current[idx] = if (itX[idx].hasNext()) itX[idx].next() else inf
       count --
